5ac05a38a4ce14e307b700e54d7d021ff947f305
cramrov <marcrvall@gmail.com>
2014-01-29 17:05:38 +0000

    Created SessionAmplify Package

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: "A Meteor Session object extension that stores the value in Amplify's local storage when set() is called. It automatically inherit all the reactive properties of Meteor Session"
+});
+
+Package.on_use(function (api, where) {
+  api.use(['underscore', 'amplify', 'session'], 'client');
+
+  api.add_files(['session_extension.js'], 'client');
+  
+  if (api.export) 
+    api.export('SessionAmplify');  
+});
